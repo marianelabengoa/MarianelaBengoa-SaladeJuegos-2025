0e947c596a2ca748301095a7a7d42b19ea47159c
marianelabengoa <marianelaluzbengoa@gmail.com>
2025-05-11 00:04:25 -0300

    pequeña correccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { DatabaseService } from './services/database.service';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { ChatComponent } from "../app/pages/chat/chat.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, HttpClientModule, CommonModule], 
+  imports: [RouterOutlet, RouterLink, HttpClientModule, CommonModule, ChatComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
 })
 export class AppComponent implements OnInit {
+  mostrarChat: boolean = false;
   usuarioLogueado: boolean = false;
   title = 'Sala De Juegos';
   databaseService = inject(DatabaseService);
@@ -28,4 +30,11 @@ export class AppComponent implements OnInit {
     await this.databaseService.logout();
     this.usuarioLogueado = false;
   }
+
+  mostrarBarraChat() {
+    this.mostrarChat = !this.mostrarChat;
+    console.log('Estado del chat:', this.mostrarChat);
+  }
+  
+  
 }
